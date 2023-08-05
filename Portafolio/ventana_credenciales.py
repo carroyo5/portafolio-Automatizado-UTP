dec0c558d6ce5b5b3a829264c2d11d3f1a8492d8
Cristhian Arroyo <arroyocristhian8@gmail.com>
2023-08-04 19:01:36 -0500

    Carga de imagen de los credenciales

--- Portafolio/ventana_credenciales.py
+++ Portafolio/ventana_credenciales.py
@@ -1,12 +1,13 @@
 import tkinter as tk
 from tkinter import filedialog
 from tkinter import messagebox
-
+from tkinter import ttk
+from PIL import Image, ImageTk
 #Configuración de la Ventana principal
-ventana = tk.Tk()
-ventana.title('PA- Portafolio automatizado')
-ventana.geometry('1280x720')
-ventana.resizable(width=False, height=False)
+credenciales = tk.Tk()
+credenciales.title('PA- Portafolio automatizado')
+credenciales.geometry('1280x720')
+credenciales.resizable(width=False, height=False)
 
 DEPARTAMENTOS = ['Arquitectura y Redes de Computadoras', ]
 
@@ -22,7 +23,7 @@ rutaTareas = tk.StringVar()
 #Mensaje de ayuda para mostrarle al usuario como se usa el programa.
 def boton_ayuda():
     #Configuracion del mensaje
-    ventana_ayuda = tk.Toplevel(ventana)
+    ventana_ayuda = tk.Toplevel(credenciales)
     ventana_ayuda.resizable(width=False, height=False)
     ventana_ayuda.title("Informacion sobre el uso del programa")
 
@@ -33,43 +34,56 @@ def boton_ayuda():
     #configuracion del mensaje de ayuda
     tk.Message(ventana_ayuda, text=mensaje, width=300).pack()
 
+#Funcion para cargar la imagen por default
+def cargar_imagen_default():
+    imagen_default = Image.open(r'.\Portafolio\imagenes\imagen_default.png')
+    imagen_default = imagen_default.resize((200, 200))
+    imagen_default = ImageTk.PhotoImage(image=imagen_default)
+    mostrar_imagen.config(image=imagen_default)
+    mostrar_imagen.image = imagen_default
+
 #Funcion para seleccionar carpetas
-def seleccion_carpeta():
-    carpeta_seleccionada = filedialog.askdirectory()
-    if carpeta_seleccionada:
-        ruta.config(text=f'Carpeta Seleccionada: {carpeta_seleccionada}')
+def seleccion_imagen():
+    imagen_seleccionada = filedialog.askopenfilename(filetypes=[('Imagenes','*.png; *.jpg; *.jpeg')])
+    if imagen_seleccionada:
+        archivo_seleccionado = tk.PhotoImage(file=imagen_seleccionada)
+        mostrar_imagen.config(image=archivo_seleccionado)
+        mostrar_imagen.image = archivo_seleccionado
     else:
-        ruta.config(text='No se ha seleccionado ninguna carpeta.')
+        cargar_imagen_default()
 
 #Texto que indica ayuda
-tk.Label(ventana, text="¿Necesitas ayuda? Pulsa este boton!").place(x=1000, y=14)
+tk.Label(credenciales, text="¿Necesitas ayuda? Pulsa este boton!").place(x=1000, y=14)
 #Configuracion del boton imagen
 imagen_ayuda = tk.PhotoImage(file = r'.\Portafolio\imagenes\info_button.png')
-tk.Button(ventana,image=imagen_ayuda, 
+tk.Button(credenciales,image=imagen_ayuda, 
           command= boton_ayuda, 
           width= 38, height=38).place(x=1200, y=0)
 #titulo
-tk.Label(ventana, text='Portafolio Automatizado', font=('Arial', 16)).pack()
-#Boton para seleccionar la ruta de la carpeta donde se quiere guardar
+tk.Label(credenciales, text='Portafolio Automatizado', font=('Arial', 16)).pack()
+imagen_buscador = tk.PhotoImage(file = r'.\Portafolio\imagenes\imagen_buscador.png')
+#Label para mostrar la imagen
+mostrar_imagen = tk.Label(credenciales, width=400, height=400)
+mostrar_imagen.pack()
+#Funcion para cargar la imagen por defecto
+cargar_imagen_default()
+#Configuracion del boton buscar imagen
+boton_buscar_imagen = ttk.Button(credenciales, text='Buscar una imagen...',
+           style='BotonPersonalizado.TButton',
+           image=imagen_buscador,
+           compound='left', 
+           command=seleccion_imagen)
+boton_buscar_imagen.pack()
 
-imagen_logo = tk.PhotoImage(file = r'.\Portafolio\imagenes\Logo.png')
-foto = tk.Label(ventana, image=imagen_logo).pack()
-
-
-
-
-tk.Button(ventana, text='Selecciona la ruta donde quieres guardar el portafolio', command=seleccion_carpeta).pack()
-ruta = tk.Label(ventana, text='')
-ruta.pack()
 #Input nombre de la materia
 tk.Label(text='Introduce el nombre de la materia').pack()
-tk.Entry(ventana, textvariable=nombreMateria).pack()
+tk.Entry(credenciales, textvariable=nombreMateria).pack()
 
 tk.Label(text='Introduce tu nombre').pack()
-tk.Entry(ventana, textvariable=nombre).pack()
+tk.Entry(credenciales, textvariable=nombre).pack()
 
 tk.Label(text='Introduce tu apellido').pack()
-tk.Entry(ventana, textvariable=apellido).pack()
+tk.Entry(credenciales, textvariable=apellido).pack()
 
 
 #Boton para crear Portafolio y comprobar la informacion proporcionada
@@ -86,4 +100,4 @@ def comprobacion():
     except Exception as MensajeMateria:
         pass
 
-ventana.mainloop()
+credenciales.mainloop()
